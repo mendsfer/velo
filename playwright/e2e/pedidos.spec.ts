3000fb9306e861d49d688f37c16c82f5aaefb6e3
Fernanda Mendes Rodrigues <fernanda.rodrigues@db1.com.br>
2026-01-23 21:33:03 -0300

    Desafio Sobreviva sem Data Test IDs na consulta de pedidos

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test'
+
+/// AAA - Arrange, Act, Assert (Preparar, Agir, Verificar)
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-DLVJ46')
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    // Assert
+    await expect(page.getByText('VLO-DLVJ46')).toBeVisible({timeout: 10_000})
+    await expect(page.getByText('APROVADO')).toBeVisible()
+})
